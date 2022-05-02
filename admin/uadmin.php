0f21ca6c1b520edc52c5849ed989b97bf12169e5
CODEKanazaki <60904060+CODEKanazaki@users.noreply.github.com>
2022-05-02 20:04:15 +0800

    added privacy policy

--- admin/uadmin.php
+++ admin/uadmin.php
@@ -1,69 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+    <head>
+        <title>User login page</title>  
+        <link rel="stylesheet" href="../css/uadmin.css">
+    </head>
+    <body class="bg">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/styles.css" />
-    <script src="https://kit.fontawesome.com/3f713e3efd.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/uadmin.css" />
-    <title>Contact</title>
-</head>
+        <header>
+            <h2>Annoucement long sentence. The quick brown fox jumps over the laze dog.</h2>
+        </header>
 
-<body>
-    <div id="announcement">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    </div>
-    <header>
-        <img src="../images/bc_logo_text.png" alt="Logo" />
-        <button id="navicon" onclick="openNav()">
-            <i class="fa-solid fa-bars fa-lg"></i>
-        </button>
-        <div id="navmenu1" class="navmenu hide">
-            <a href="index.php">Home</a>
-            <a href="explore.html">Explore</a>
-            <a href="rewards.html">Rewards</a>
-            <a href="about.html">About</a>
-            <a href="contact.html" class="active">Contact</a>
-        </div>
-    </header>
+    <div class="content">
 
-    <div class="container">
-        <div id="login-div" class="content">
-            <h1>Left Side</h1>
-            <form>
-                <label for="username">Username </label>
-                <input id="username" type="text" />
-                <br />
-                <label for="pass">Password </label>
-                <input id="pass" type="text" />
-                <br />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-        <div id="login-google-div" class="content">
-            <h1>Right Side</h1>
+        <div class="box1">
+            <h2>Sign in</h2>
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+        <label>
+
+
+        <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+        </label>
+        
+        <span class="psw"><a href="#">Forgot password?</a></span>
 
         </div>
+
+        <div class="box2">
+        <span class="vl-innertext">or</span>
+        </div>
+           
     </div>
 
-    <footer>
-        <div>
-            <p>© 2022 Bueno Central</p>
-            <p>Privacy Policy</p>
-        </div>
-    </footer>
-    <script>
-        const openNav = () => {
-            var x = document.getElementById("navmenu1");
-            if (x.className === "navmenu") {
-                x.className += " hide";
-            } else {
-                x.className = "navmenu";
-            }
-        };
-    </script>
-</body>
+        <!-- <div class="box2">
+        <img src="../images/bc_logo_text.png">
+        </div> -->
 
+
+
+        <footer> 
+            <div class="wave">
+            <img src="../images/wave.svg">
+            </div>
+        
+        </footer>
+        
+    </body>
+
+    <script src="" async defer></script>
 </html>
--- admin/user_admin.php
+++ admin/user_admin.php
@@ -6,10 +6,34 @@
     </head>
     <body>
     
+
+
+       
+
+
+
         <header>
             <h2>Annoucement long sentence. The quick brown fox jumps over the laze dog.</h2>
         </header>
+
+        <div class="box1">lorem</div>
+
+
         
+        <!-- <div class="wrapper">
+            <div class="buenoLogo">
+            <img src="../images/bc_logo_text.png" alt="Logo">
+            </div>
+            <div class="nav-area">
+                <li><a href="#">Home</li>
+                <li><a href="#">Explore</li>
+                <li><a href="#">Rewards</li>
+                <li><a href="#">About</li>
+                <li><a href="#">Contact</li>
+            </div>
+
+
+        </div> -->
         
         
         <footer> 
